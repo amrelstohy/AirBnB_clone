3d096bca23d241dbe1c7fbe6ea686ad2b0bc95af
amrelstohy <amramr2901@gmail.com>
2024-03-16 10:42:25 +0200

    hi

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 console module
 """
 import cmd
+from models import base_model
+from models.engine import file_storage
+
 
 class HBNBCommand(cmd.Cmd):
 
@@ -23,6 +26,58 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line function"""
         pass
+
+    def do_creat(self, *args):
+        """creat new object"""
+        print(args)
+        if (args[0] == "BaseModel"):
+            x = base_model.BaseModel()
+            x.save()
+            print(x.id)
+        elif (args[0] == ''):
+            print("** class name missing **")
+        else :
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        a = 0
+        args = line.split()
+        if (len(args == 0)):
+            print("** class name missing **")
+        elif (len(args == 1)):
+            print("** instance id missing **")
+        elif (len(args > 1)):
+            if (args[0] == "BaseModel"):
+                x = file_storage.FileStorage.all()
+                for key, value in x.items():
+                    if (key = )
+            else :
+                print("** class doesn't exist **")
+
+
+
+
+
+
+
+        if (args[0] == "BaseModel"):
+            if (len(args) == 2):
+                id = args[0] + '.' + args[1]
+                x = file_storage.FileStorage.all(self)
+                for key, value in x.items():
+                    if (key == id):
+                        a = value
+                if (a):
+                    print(a)
+                else:
+                    print("** no instance found **")
+            elif (len(args) == 1):
+                print("** instance id missing **")
+        elif (args[0] == ''):
+            print("** class name missing **")
+        else :
+            print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
