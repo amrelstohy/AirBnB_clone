791aa1f511ff7ffca88b242e1262de1598be4576
amrelstohy <amramr2901@gmail.com>
2024-03-16 04:39:49 +0200

    hi

--- console.py
+++ console.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
+"""
+console module
+"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-    
+    """
+    the class of console
+    """
     
     def do_quit(self, line):
         """quit command to exit the program"""
