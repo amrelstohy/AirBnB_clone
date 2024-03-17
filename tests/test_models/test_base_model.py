2f082f85052f37994135e4d8de9ad0c3f10483b8
amrelstohy <amramr2901@gmail.com>
2024-03-17 02:32:43 +0200

    hi

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,14 +5,19 @@ this module tests Base_model.py
 
 from models.base_model import BaseModel
 import unittest
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     """this class tests BaseModel class"""
 
-    def test_id(self):
+    def test_init(self):
         x = BaseModel()
-        x.id = "123"
-        self.assertEqual(x.id, "123")
+        self.assertEqual(type(x.id), str)
+        self.assertEqual(len(x.id), 36)
+        self.assertEqual(type(x.created_at), datetime)
+        self.assertEqual(type(x.updated_at, datetime))
+
+    
 
     def test_name(self):
         x = BaseModel()
