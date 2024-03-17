fe933a3076cc37b60040dfe7220f21bf281eee1c
amrelstohy <amramr2901@gmail.com>
2024-03-17 06:58:05 +0200

    hi

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this module tests user.py
+"""
+
+from models.user import User
+import unittest
+
+class TestUser(unittest.TestCase):
+    """this class test User class"""
+
+    def test_vars(self):
+        x = User()
+        self.assertEqual(type(x.password), str)
+        self.assertEqual(type(x.email), str)
+        self.assertEqual(type(x.first_name), str)
+        self.assertEqual(type(x.last_name), str)
